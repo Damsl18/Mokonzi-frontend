b118bc8ebad9f59bccfad8518ad2fc94a1b406db
Damsl18 <davidshamba18@gmail.com>
2026-06-12 23:20:20 +0100

    Ajout des variables d'environnements

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
